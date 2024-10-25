07625120910a22744744070862b41487087fdef7
Hoangcanhh <cnhwoang11@gmail.com>
2024-10-25 11:35:22 +0700

    firt-commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common';
 import { playerModule } from './player/player.module';
 import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [playerModule]
 })
 @Module({
   imports: [ConfigModule.forRoot()]
+})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
+})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
